83caa1498993b29dc4c4261f13710ff3810fc4df
Project {'Lana'} <73310001+LanaTheProtogen@users.noreply.github.com>
2021-02-06 02:22:57 -0300

    Updated all html files and added voice recognition.

--- PROJECT_LANA.js
+++ PROJECT_LANA.js
@@ -1,3 +1,6 @@
+const button = document.querySelector(".LANA");
+const reply = document.querySelector(".LANA-REPLY");
+
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -9,3 +12,43 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
   }
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = () => {
+  console.log("Hey it's me Lana :3");
+  reply.textContent = "{ Hey it's me Lana :3 }"
+};
+
+recognition.onresult = (event) => {
+  const current = event.resultIndex;
+  const transcript = event.results[current][0].transcript;
+  console.log(transcript);
+  if (transcript.includes('Hey Lana')) {
+    reply.textContent = `{ Hey it's me :3 }`;
+  }
+  if (transcript.includes('Lana how are you')) {
+    reply.textContent = `{ I'm alright, thanks for asking :3 }`;
+  }
+  if (transcript.includes("Lana what are you")) {
+    reply.textContent = `{ I'm a protogen ~w~ }`;
+  }
+  //lanaRead(transcript);
+};
+
+button.addEventListener('click', () => {
+  recognition.start();
+});
+
+/*
+function lanaRead(message){
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+  speech.volume = 0.8;
+  speech.rate = 0.8;
+  speech.pitch = 1;
+  window.speechSynthesis.speak(speech);
+}
+*/
